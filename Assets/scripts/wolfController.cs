8ce1cc25dac72c239cdbd2f444ec3003ff73cc30
Croatin <adriana.gabrielle.rivera@gmail.com>
2017-03-29 11:06:16 -0400

    I believe this commit has all my terrain changes.

---
+++ Assets/scripts/wolfController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wolfController : MonoBehaviour
+{
+    private NewtonVR.NVRHand paw;
+    private NewtonVR.NVRPlayer vrPlayer;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        while(paw.HoldButtonPressed)
+        {
+
+        }
+	}
+}
